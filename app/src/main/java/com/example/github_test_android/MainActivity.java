bdb424bb439ba80205e957b29ffeb6b906c961b3
NightOwl <lhh80482@gmail.com>
2019-02-12 12:05:32 +0900

    hello World

--- app/src/main/java/com/example/github_test_android/MainActivity.java
+++ app/src/main/java/com/example/github_test_android/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView a = new TextView(this);
-        a.setText("Hello World, My Home");
+        a.setText("Hello World, My Home");cxc
         setContentView(a);
     }
 }
